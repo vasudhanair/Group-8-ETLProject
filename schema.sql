fec74cda182f2852b3e6bb123ba77160a50a9056
Eddie <eddie.xu.nyc@gmail.com>
2020-11-03 20:47:45 -0500

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,29 +1,28 @@
 -- Create tables for raw data to be loaded into
-CREATE TABLE Weather_data (
-City TEXT,
-MaxTemp BIGINT, 
-Humidity INT,
-Cloudiness INT,
-WindSpeed BIGINT,
-Date DATE NOT NULL,
-CONSTRAINT pk_Weather_data PRIMARY KEY (City)
+CREATE TABLE weather_data (
+city TEXT,
+max_temp BIGINT,
+humidity INT,
+cloudiness INT,
+wind_speed BIGINT,
+date TEXT NOT NULL
 );
-
-CREATE TABLE Starbucks_location (
-StoreNumber TEXT PRIMARY KEY,
-StoreName VARCHAR(255) NOT NULL,
-OwnershipType TEXT,
-StreetAddress VARCHAR NOT NULL,
-City TEXT,
-STATEProvince VARCHAR,
-Postcode INT,
-PhoneNumber TEXT
+select * from starbucks_location
+CREATE TABLE starbucks_location (
+store_number TEXT,
+store_name VARCHAR(255) NOT NULL,
+ownership_type TEXT,
+street_address TEXT NOT NULL,
+city TEXT,
+state_province VARCHAR,
+postcode TEXT,
+phone_number TEXT
 );
 
 -- Joins tables
-SELECT Weather_data.City, Weather_data.MaxTemp, Weather_data.Humidity, Weather_data.Cloudiness, Weather_data.WindSpeed,Weather_data.Date,
-Starbucks_location.StoreNumber,Starbucks_location.StoreName,Starbucks_location.OwnershipType,Starbucks_location.StreetAddress,Starbucks_location.STATEProvince,
-Starbucks_location.Postcode,Starbucks_location.PhoneNumber
-FROM Weather_data
-JOIN Starbucks_location
-ON Weather_data.City=Starbucks_location.City;
+SELECT weather_data.city, weather_data.max_temp, weather_data.humidity, weather_data.cloudiness, weather_data.wind_speed,weather_data.date,
+starbucks_location.store_number,starbucks_location.store_name,starbucks_location.ownership_type,starbucks_location.street_address,starbucks_location.state_province,
+starbucks_location.postcode,starbucks_location.phone_number
+FROM weather_data
+JOIN starbucks_location
+ON weather_data.city=starbucks_location.city;
